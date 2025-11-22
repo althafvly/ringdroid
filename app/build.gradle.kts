10ce04f44e8729fa6213e42e946d4322dea2fac8
althafvly <althafvly@gmail.com>
2025-11-22 23:48:40 +0530

    Bump version to v2.7.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.ringdroid"
         minSdk = 21
         targetSdk = 36
-        versionCode = 20708
-        versionName = "2.7.8"
+        versionCode = 20709
+        versionName = "2.7.9"
     }
 
     buildTypes {
